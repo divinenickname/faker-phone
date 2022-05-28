d274d429df371be5a011cb96a24fbfab20d0e5d8
a.Ilinykh <divinenickname@gmail.com>
2022-05-28 10:19:26 +0500

    fix number generator call

--- src/main/java/ru/divinenickname/kotlin/faker/phone/Phone.kt
+++ src/main/java/ru/divinenickname/kotlin/faker/phone/Phone.kt
@@ -5,14 +5,13 @@ import java.io.File
 import java.util.*
 
 class Phone(
-    private val locale: Locale,
+    locale: Locale,
 ) {
     private val objectMapper = ObjectMapperYaml()
-    val a = locale.isO3Country
-    private val file = File(this.javaClass.getResource("/locales/ru_RU.yaml").path)
+    private val file = File(this.javaClass.getResource("/locales/${locale.language}.yaml").path)
     private val properties = objectMapper.readValue(file, Properties::class.java)
 
-    val number = Number(properties.format).number
+    val number = Number(properties.normalizedFormat).number
     val international = "${properties.codes.country}$number"
     val trunk = "${properties.codes.trunk}$number"
 }
