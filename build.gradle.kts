94cc672301a20d9f332d4e4c36b1fb79fde08795
Alexander Ilinykh <divinenickname@gmail.com>
2022-05-28 08:57:21 +0500

    ISSUE-7: move version from build gradle to other file (#8)
    * ISSUE-7: move version from build gradle to other file
    
    * ISSUE-7: add Dependencies.kt file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     idea
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version Versions.KOTLIN
 }
 
 group = "ru.divinenickname.kotlin.faker.phone"
@@ -12,9 +12,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:5.3.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:${Versions.JUNIT}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${Versions.JUNIT}")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:${Versions.KOTEST}")
 }
 
 tasks.withType<Test> {
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,6 @@
+object Versions {
+    const val KOTLIN = "1.6.21"
+    const val JACKSON = "2.13.3"
+    const val JUNIT = "5.8.2"
+    const val KOTEST = "5.3.0"
+}
